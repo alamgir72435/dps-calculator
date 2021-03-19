e437fe5d038a7237333f4818d0423a77c7397088
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-03-19 14:48:02 +0600

    dps calculator ready

--- ndps.js
+++ ndps.js
@@ -1,15 +1,17 @@
-const initialAmount = 1000;
-const rate = 8;
-const total_month = 24;
+function dps_calc(initialAmount, rate, total_month) {
+  var temp = Math.pow(rate / 100 + 1, 1 / 12);
+  var fx =
+    (Math.pow(rate / 100 + 1, total_month / 12) - 1) / ((temp - 1) / temp);
+  /*[(r/100 + 1)m/12 - 1 ] / [{(r/100+1)1/12-1}/(r/100+1)1/12]*/
+  var amount_at_maturity = initialAmount * fx;
+  var totalProfit = amount_at_maturity - initialAmount * total_month;
+  let a = Math.pow(rate / 100 + 1, total_month / 12) - 1;
+  return {
+    maturity: amount_at_maturity.toFixed(2),
+    profit: totalProfit.toFixed(2),
+  };
+}
 
-// ......................................
-var temp4 = Math.pow(rate / 100 + 1, 1 / 12);
-var temp8 =
-  (Math.pow(rate / 100 + 1, total_month / 12) - 1) / ((temp4 - 1) / temp4); /*[(r/100 + 1)m/12 - 1 ] / [{(r/100+1)1/12-1}/(r/100+1)1/12]*/
+let result = dps_calc(1000, 8, 12);
 
-var amount_at_maturity = initialAmount * temp8;
-var totalProfit = amount_at_maturity - initialAmount * total_month;
-
-//=============================
-console.log(amount_at_maturity);
-console.log(totalProfit);
+console.log(result);
